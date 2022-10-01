446601c1a0fffba373375d847420c6464de236a7
Ruthwik <bgruthwik@gmail.com>
2022-09-30 20:54:00 -0400

    first commit

---
+++ src/model/Employee.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Rutu
+ */
+public class Employee {
+    
+    private String name;
+    private String empID;
+    private String empAge;
+    private String gender;
+    private String empStartDate;
+    private String empLevel;
+    private String empTeamInfo;
+    private String emailAddress;
+    private String cellPhoneNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(String empAge) {
+        this.empAge = empAge;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmpStartDate() {
+        return empStartDate;
+    }
+
+    public void setEmpStartDate(String empStartDate) {
+        this.empStartDate = empStartDate;
+    }
+
+    public String getEmpLevel() {
+        return empLevel;
+    }
+
+    public void setEmpLevel(String empLevel) {
+        this.empLevel = empLevel;
+    }
+
+    public String getEmpTeamInfo() {
+        return empTeamInfo;
+    }
+
+    public void setEmpTeamInfo(String empTeamInfo) {
+        this.empTeamInfo = empTeamInfo;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getCellPhoneNo() {
+        return cellPhoneNo;
+    }
+
+    public void setCellPhoneNo(String cellPhoneNo) {
+        this.cellPhoneNo = cellPhoneNo;
+    }
+    
+    
+    
+}
